5dcff9db5a7413ae9300593686311877641f32b2
Dirk Arnez <smalldirkalex@gmail.com>
2024-08-17 17:40:35 +0800

    - update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
  
 int main()
 {
-    std::system("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -command \"Add-Type -AssemblyName System.Speech;$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;$voiceoptions = $speak.GetInstalledVoices(\\\"en-US\\\");$voice = $voiceoptions[$voicenum % $voiceoptions.count];$speak.SelectVoice($voice.VoiceInfo.Name);$speak.Rate=\\\"0\\\";$speak.Speak(\\\"dfgdfgdfgd\\\");\""); // executes the UNIX command "ls -l >test.txt"
-    std::cout << std::ifstream("test.txt").rdbuf();
+ /* cmd.exe
+ C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command "Add-Type -AssemblyName System.Speech;$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;$voiceoptions = $speak.GetInstalledVoices(\"en-US\");$voice = $voiceoptions[$voicenum % $voiceoptions.count];$speak.SelectVoice($voice.VoiceInfo.Name);$speak.Rate=\"0\";$speak.Speak(\"dfgdfgdfgd\");"
+ */
+ std::system("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -command \"Add-Type -AssemblyName System.Speech;$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;$voiceoptions = $speak.GetInstalledVoices(\\\"en-US\\\");$voice = $voiceoptions[$voicenum % $voiceoptions.count];$speak.SelectVoice($voice.VoiceInfo.Name);$speak.Rate=\\\"0\\\";$speak.Speak(\\\"dfgdfgdfgd\\\");\""); // executes the UNIX command "ls -l >test.txt"
+ std::cout << std::ifstream("test.txt").rdbuf();
 }
